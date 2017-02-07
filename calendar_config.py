29a7a0d1f8ab02d9b32986566d0bd29737fadd41
neotea <hans-mustermann@web.de>
2017-02-07 16:10:06 +0100

    add gitignore

--- calendar_config.py
+++ calendar_config.py
@@ -1,7 +1,7 @@
 SCOPE = 'https://www.googleapis.com/auth/calendar'
 USER_AGENT = 'GoogleCalendarDisplay/1.0'
-CLIENT_ID = ''
-CLIENT_SECRET = ''
-DEVELOPER_KEY = ''
+CLIENT_ID = '409255160896-qq33qf34uvlq97p9052rblvj47q5lq58.apps.googleusercontent.com'
+CLIENT_SECRET = '9OmtCJbXhOdnb-XXMc3zhIri'
+DEVELOPER_KEY = 'AIzaSyCadP0t4oOWPFyOm-0uMU3OQ-bbe3dphVk'
 
-CALENDAR_IDS = ['room1@resource.calendar.google.com','room2@resource.calendar.google.com','room3@resource.calendar.google.com']
+CALENDAR_IDS = ['collect.ai_32383734373032392d313330@resource.calendar.google.com','collect.ai_32383734373032392d313330@resource.calendar.google.com','collect.ai_34333935333330352d353538@resource.calendar.google.com','collect.ai_2d3832323130333035313534@resource.calendar.google.com']
